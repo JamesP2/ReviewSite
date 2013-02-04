5663fe597804c4be4b29a8b80fd947a380255732
James Phillips <james@thefailboat.co.uk>
2013-02-04 22:41:01 +0000

    Start role editor

---
+++ Review-Site/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Review_Site.Areas.Admin.Models;
+using Review_Site.Models;
+
+namespace Review_Site.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        private SiteContext db = new SiteContext();
+
+
+        //
+        // GET: /Admin/Role/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Admin/Role/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Admin/Role/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Admin/Role/Create
+
+        [HttpPost]
+        public ActionResult Create(Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                role.ID = new Guid();
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Edit", role.ID);
+            }
+            return View(role);
+        }
+        
+        //
+        // GET: /Admin/Role/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            ViewBag.PermissionList = GetPermissionList();
+            Role role = db.Roles.Single(x => x.ID == id);
+            return View(roleToForm(role));
+        }
+
+        //
+        // POST: /Admin/Role/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Guid id, RoleForm collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<SelectListItem> GetPermissionList()
+        {
+            List<SelectListItem> list = new List<SelectListItem>();
+            foreach (Permission p in db.Permissions)
+            {
+                list.Add(new SelectListItem
+                {
+                    Value = p.ID.ToString(),
+                    Text = p.Name + "("+p.Identifier+")"
+                });
+            }
+            return list;
+        }
+
+        private RoleForm roleToForm(Role role)
+        {
+            IList<Guid> permissionGuids = new List<Guid>();
+            foreach (Permission p in role.Permissions)
+            {
+                permissionGuids.Add(p.ID);
+            }
+            return new RoleForm()
+            {
+                ID = role.ID,
+                SelectedPermissionIds = permissionGuids,
+                Name = role.Name
+            };
+        }
+
+        ////
+        //// GET: /Admin/Role/Delete/5
+ 
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Admin/Role/Delete/5
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+ 
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Review-Site/Areas/Admin/Models/RoleForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Review_Site.Areas.Admin.Models
+{
+    public class RoleForm
+    {
+        public Guid ID { get; set; }
+        [Required(ErrorMessage = "Please provide a Name for the Role.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Permissions of this Role(Hold Control to select multiple Permissions)")]
+        public IList<Guid> SelectedPermissionIds { get; set; }
+    }
+}
