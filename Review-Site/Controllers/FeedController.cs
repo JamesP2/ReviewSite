3ccc25454994e7228187855a911d9f640c0750a6
James Phillips <james@thefailboat.co.uk>
2013-02-05 20:43:58 +0000

    RSS Feed Models to start implementing #26

---
+++ Review-Site/Controllers/FeedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Review_Site.Controllers
+{
+    public class FeedController : Controller
+    {
+        //
+        // GET: /Feed/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Review-Site/Models/Feed/Feed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Review_Site.Models.Feed
+{
+	public class Feed
+	{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+
+        public DateTime lastBuildDate { get; set; }
+        public DateTime pubDate { get; set; }
+
+        public int ttl { get; set; }
+	}
+}
