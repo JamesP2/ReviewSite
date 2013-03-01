0f8bf914ec132a985f1bb6f0a7bc6a4790e4372c
James Phillips <james@thefailboat.co.uk>
2013-03-01 00:05:58 +0000

    Converter for EF to NH

---
+++ Review-Site.Converter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Review_Site.Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EF to NH Database Conversion");
+            Console.WriteLine("");
+            //TODO: Verify connection strings exist, import them and go do stuff!
+        }
+    }
+}
